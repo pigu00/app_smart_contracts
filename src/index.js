223243795a07fc0473dae8fe5e9d27431ee7e798
Roberto Marban <robertomarban@gmail.com>
2022-05-10 12:31:32 -0300

    app_smart_contract con json

--- src/index.js
+++ src/index.js
@@ -3,16 +3,31 @@ import ReactDOM  from 'react-dom'
 import "bootstrap/dist/css/bootstratp.min.css"
 import detectEthereumProvider from "@metamask/detect-provider"
 import {Contract, ethers} from 'ethers'
+import myContractManifest from './contracs/MyContract.json'
 
 class App extends React.Component{
 
     async getBlockchain(){
-        let provider = await detectEthereumProvider
+
+        let provider = await detectEthereumProvider()
+
         if (provider){
           await provider.request( { method: 'eht_requestAccounts' } )
-        provider = 
+        provider = new ethers.providers.Web3Provider(provider);
+        const signer = provider.getSigner()
+
+            const myContract = new Contract(
+                myContractManifest.networks[97].address,
+                myContractManifest.abi,
+                signer
+
+            )
+            return myContract;
+
         }
+
         return null;
+
 
     }
     
